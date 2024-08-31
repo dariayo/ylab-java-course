5d413527f076ba841f7e24d1ac99346c853f547b
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2024-08-31 20:28:46 +0300

    fix naming

--- ylab/src/main/java/ru/dariayo/controllers/OrderController.java
+++ ylab/src/main/java/ru/dariayo/controllers/OrderController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.dariayo.dto.OrderDTO;
+import ru.dariayo.dto.OrderRequestDTO;
 import ru.dariayo.mapper.OrderMapper;
 import ru.dariayo.model.Order;
 import ru.dariayo.repositories.OrderCollection;
@@ -25,18 +26,19 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<?> makeOrder(@RequestParam String buyerName, @RequestParam String carMark) {
-        if (buyerName == null || carMark == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Request body is missing");
+    public ResponseEntity<?> createOrder(@RequestBody OrderRequestDTO orderRequest) {
+        if (orderRequest.getBuyerName() == null || orderRequest.getBuyerName().isBlank() ||
+                orderRequest.getCarMark() == null || orderRequest.getCarMark().isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Buyer name and car mark are required");
         }
-        Order order = new Order(buyerName, carMark, "PENDING");
-        orderRepository.makeOrder(buyerName, carMark);
+        Order order = new Order(orderRequest.getBuyerName(), orderRequest.getCarMark(), "PENDING");
+        orderRepository.makeOrder(orderRequest.getBuyerName(), orderRequest.getCarMark());
         OrderDTO orderDTO = orderMapper.orderToOrderDTO(order);
         return ResponseEntity.status(HttpStatus.CREATED).body(orderDTO);
     }
 
     @PutMapping("/changeStatus/{id}")
-    public ResponseEntity<?> changeStatus(@PathVariable Integer id, @RequestParam String status) {
+    public ResponseEntity<?> updateOrderStatus(@PathVariable Integer id, @RequestParam String status) {
         if (status == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Request body is missing");
         }
@@ -54,7 +56,7 @@ public class OrderController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<?> searchOrder(@RequestParam String param, @RequestParam String value) {
+    public ResponseEntity<?> findOrderById(@RequestParam String param, @RequestParam String value) {
         if (param == null || value == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Request body is missing");
         }
